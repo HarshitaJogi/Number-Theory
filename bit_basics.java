7b6049c4f28834e9aa8a8e568d39f41ac0001d05
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-02-11 23:55:57 +0530

    Add files via upload

---
+++ bit_basics.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class bit_basics{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        int a = 6;
+        printBinary(a);
+
+        // to check if 2nd bit is set or not
+        if((a & (1<<2)) != 0){
+            System.out.println("Bit Set");
+        }
+        else System.out.println("Bit not set");
+        
+        // to set 8th bit as 1
+        printBinary(a | (1<<8));
+
+        // to unset 2nd bit
+        printBinary(a & ~(1<<2));
+
+
+	}
+
+    static void printBinary(int num){
+        for(int i=10; i>=0; i--){
+            System.out.print( (num>>i) & 1 );
+        }
+        System.out.println();
+    }
+}
